c81f04b3913ac92b9c1e77cc66240747465fbb4f
MONY <MONY@MONY-PC>
2015-11-23 10:46:28 -0500

    ingreso de clientes implementado

--- src/main/java/com/daraf/projectdarafapp/facade/AppFacade.java
+++ src/main/java/com/daraf/projectdarafapp/facade/AppFacade.java
@@ -8,6 +8,8 @@ package com.daraf.projectdarafapp.facade;
 import com.daraf.projectdarafprotocol.DBClient;
 import com.daraf.projectdarafprotocol.appdb.MensajeRQ;
 import com.daraf.projectdarafprotocol.appdb.MensajeRS;
+import com.daraf.projectdarafprotocol.appdb.ingresos.IngresoClienteRQ;
+import com.daraf.projectdarafprotocol.appdb.ingresos.IngresoClienteRS;
 import com.daraf.projectdarafprotocol.appdb.seguridades.AutenticacionEmpresaRQ;
 import com.daraf.projectdarafprotocol.appdb.seguridades.AutenticacionEmpresaRS;
 import com.daraf.projectdarafprotocol.model.Empresa;
@@ -34,5 +36,25 @@ public class AppFacade {
             }
         }
         return null;
+    }
+
+    public static Boolean insernewclient(String id, String nombre, String direccion, String telefono)
+    {
+        DBClient dbclient =new DBClient();
+        MensajeRQ msj = new MensajeRQ("appserver",MensajeRQ.ID_MENSAJE_INGRESOCLIENTE);
+        IngresoClienteRQ ing =new IngresoClienteRQ();
+        ing.setId(id);
+        ing.setNombre(nombre);
+        ing.setDireccion(direccion);
+        ing.setTelefono(telefono);
+        msj.setCuerpo(ing);
+        MensajeRS response = dbclient.sendRequest(msj);
+        IngresoClienteRS ingrs=(IngresoClienteRS) response.getCuerpo();
+        if(ingrs.getResultado().equals("1")){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
--- src/main/java/com/daraf/projectdarafapp/server/AppSocketSession.java
+++ src/main/java/com/daraf/projectdarafapp/server/AppSocketSession.java
@@ -14,6 +14,8 @@ import com.daraf.projectdarafprotocol.Mensaje;
 import com.daraf.projectdarafprotocol.clienteapp.seguridades.AutenticacionEmpresaRQ;
 import com.daraf.projectdarafprotocol.clienteapp.MensajeRQ;
 import com.daraf.projectdarafprotocol.clienteapp.MensajeRS;
+import com.daraf.projectdarafprotocol.clienteapp.ingresos.IngresoClienteRQ;
+import com.daraf.projectdarafprotocol.clienteapp.ingresos.IngresoClienteRS;
 import com.daraf.projectdarafprotocol.clienteapp.seguridades.AutenticacionEmpresaRS;
 import com.daraf.projectdarafprotocol.model.Empresa;
 import java.io.BufferedReader;
@@ -69,6 +71,21 @@ public class AppSocketSession extends Thread {
                         }
 
                         mensajeRS.setCuerpo(autRS);
+                        output.write(mensajeRS.asTexto() + "\n");
+                        output.flush();
+                    }
+                    if(msj.getCabecera().getIdMensaje().equals(Mensaje.ID_MENSAJE_INGRESOCLIENTE))
+                    {
+                        IngresoClienteRQ ing = (IngresoClienteRQ) msj.getCuerpo();
+                        Boolean ingresocorrecto =AppFacade.insernewclient(ing.getId(),ing.getNombre(),ing.getDireccion(),ing.getTelefono());
+                        MensajeRS mensajeRS = new MensajeRS("appserver",Mensaje.ID_MENSAJE_INGRESOCLIENTE);
+                        IngresoClienteRS ingrs =new IngresoClienteRS();
+                        if(ingresocorrecto)
+                           ingrs.setResultado("1");
+                        else{
+                            ingrs.setResultado("2");
+                        }
+                        mensajeRS.setCuerpo(ingrs);
                         output.write(mensajeRS.asTexto() + "\n");
                         output.flush();
                     }
